50cf3c5beb29de85cd07c3dfc544d065ba1ee743
Anthony <anthonyreagan126@gmail.com>
2025-06-07 17:14:38 +0300

    implement pagination and filtering for messages

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -62,8 +62,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',  # For browser-based API usage
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication'
-      ]
-
+      ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 ROOT_URLCONF = 'messaging_app.urls'
