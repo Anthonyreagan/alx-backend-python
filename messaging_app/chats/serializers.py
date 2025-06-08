5aea1fb8f50b97d46fed9071e8bb101a449723ef
Anthony <anthonyreagan126@gmail.com>
2025-06-08 09:32:44 +0300

    setting up serializers

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,70 @@
+from rest_framework import serializers
+from .models import User, Conversation, Message
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'user_id',
+            'email',
+            'first_name',
+            'last_name',
+            'phone_number',
+            'profile_picture',
+            'status',
+            'last_seen'
+        ]
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'profile_picture': {'required': False}
+        }
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = [
+            'message_id',
+            'sender',
+            'message_body',
+            'sent_at',
+            'read'
+        ]
+        read_only_fields = ['sent_at']
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = [
+            'conversation_id',
+            'participants',
+            'created_at',
+            'updated_at',
+            'messages'
+        ]
+        read_only_fields = ['created_at', 'updated_at']
+
+
+class ConversationCreateSerializer(serializers.ModelSerializer):
+    participants = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=User.objects.all(),
+        write_only=True
+    )
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants']
+
+    def create(self, validated_data):
+        participants = validated_data.pop('participants')
+        conversation = Conversation.objects.create(**validated_data)
+        conversation.participants.set(participants)
+        return conversation
